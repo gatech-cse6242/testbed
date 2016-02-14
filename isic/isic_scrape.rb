32da74a549b45936eb7cb70d13114fac57439d48
Matt May <matthewbentonmay@gmail.com>
2016-02-14 11:45:53 -0600

    made multithreaded

--- isic/isic_scrape.rb
+++ isic/isic_scrape.rb
@@ -1,5 +1,5 @@
-
 require 'open-uri'
+require 'json'
 
 def retrieve_as_json(url)
   JSON.parse(retrieve_data(url))
@@ -40,31 +40,31 @@ end
 
 ground_truth = File.open('isic_ground_truth.csv', 'w')
 
-list_of_images.each_with_index do |image, indx| 
+list_of_images.each_with_index do |image, indx|
   puts "Retrieving image #{indx} in result set."
-  
+
   image_id = image['_id']
-  
+
   metadata = get_metadata(image_id)
   clinical_metadata = metadata['meta']['clinical']
-  
+
   metadata_file = "./isic_metadata/#{image_id}"
   File.open(metadata_file, 'w') { |f| f.write(clinical_metadata.to_json) }
   puts "Wrote metadata to file - #{metadata_file}."
-  
+
   res = ['ben_mal', 'benign_malignant', 'malignant'].any? do |possible_key|
     if (result = clinical_metadata[possible_key])
       ground_truth.puts("#{image_id},#{result}")
       puts "Wrote class #{result} to file for #{image_id}."
-      
+
       true
     else
       false
     end
   end
-  
+
   raise "Image #{image_id} did not contain one of the required keys." if !res
-  
+
   sleep(0.01)
 end
 
@@ -72,22 +72,33 @@ ground_truth.close
 
 puts list_of_images.count
 
-def download_image(id)
-  pattern = -> (id) { "https://isic-archive.com:443/api/v1/image/558d6087bae47801cf734375/download" }
-  retrieve_data(pattern.call(id))
+def download_image(image_id)
+  pattern = -> (id) { "https://isic-archive.com:443/api/v1/image/#{id}/download" }
+  retrieve_data(pattern.call(image_id))
 end
 
-list_of_images.each_with_index do |image, indx| 
-  puts "Retrieving image #{indx} in result set."
-  
-  image_id = image['_id']
-  
-  image_file = "./isic_images/#{image_id}"
-  File.open(image_file, 'wb') { |f| f.write(download_image(image_id)) }
-  
-  puts "Wrote image to file - #{image_file}."
-  
-  sleep(0.01)
+batched_list = list_of_images.each_slice(339).to_a
+
+threads = batched_list.map do |list|
+  Thread.new(list) do |list|
+    list.each_with_index do |image, indx|
+      puts "Retrieving image #{indx} in result set."
+
+      image_id = image['_id']
+
+      image_file = "./isic_images/#{image_id}"
+
+      begin
+        File.open(image_file, 'wb') { |f| f.write(download_image(image_id)) }
+        # puts "Wrote image to file - #{image_file}."
+      rescue
+        puts "Failed to grab #{image_id}."
+        next
+      end
+
+      sleep(0.01)
+    end
+  end
 end
 
-
+threads.each { |t| t.join }
